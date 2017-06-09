23070f120c06867ef2c3dcc48b0db82905719eff
Seb <fleatch64>
2017-06-09 12:23:07 -0700

    Fix pls

--- LeagueOfYeah/AIO.cpp
+++ LeagueOfYeah/AIO.cpp
@@ -15,66 +15,149 @@ AIO::~AIO()
 	ChampionMenu->Remove();
 }
 
-void AIO::OnGameUpdate()
+void AIO::OnOrbwalkBeforeAttack(IUnit* Target)
+{
+}
+
+PLUGIN_EVENT(void) OnOrbwalkAttack(IUnit* Source, IUnit* Target)
 {
 
 }
 
-void AIO::OnRender()
+PLUGIN_EVENT(void) OnOrbwalkAfterAttack(IUnit* Source, IUnit* Target)
 {
 
 }
 
-void AIO::OnRealSpellCast(CastedSpell const& Args)
+// Return an IUnit object here to force the orbwalker to select it for this tick
+PLUGIN_EVENT(IUnit*) OnOrbwalkingFindTarget()
+{
+	return nullptr;
+}
+
+PLUGIN_EVENT(void) OnOrbwalkTargetChange(IUnit* OldTarget, IUnit* NewTarget)
 {
 
 }
 
-void AIO::OnSpellCast(CastedSpell const& Args)
+PLUGIN_EVENT(void) OnOrbwalkNonKillableMinion(IUnit* NonKillableMinion)
 {
 
 }
-bool AIO::OnPreCast(int Slot, IUnit* Target, Vec3* StartPosition, Vec3* EndPosition)
+
+PLUGIN_EVENT(void) OnGameUpdate()
+{
+
+}
+
+PLUGIN_EVENT(void) OnRender()
+{
+
+}
+
+PLUGIN_EVENT(void) OnSpellCast(CastedSpell const& Args)
+{
+
+}
+
+PLUGIN_EVENT(void) OnUnitDeath(IUnit* Source)
+{
+
+}
+
+PLUGIN_EVENT(void) OnCreateObject(IUnit* Source)
+{
+
+}
+
+PLUGIN_EVENT(void) OnDestroyObject(IUnit* Source)
+{
+
+}
+
+PLUGIN_EVENT(void) OnDoCast(CastedSpell const& Args)
+{
+}
+
+PLUGIN_EVENT(void) OnInterruptible(InterruptibleSpell const& Args)
+{
+
+}
+
+PLUGIN_EVENT(void) OnGapCloser(GapCloserSpell const& Args)
+{
+
+}
+
+// Called when issuing an order (e.g move, attack, etc.)
+// Return false to stop order from executing
+PLUGIN_EVENT(bool) OnIssueOrder(IUnit* Source, DWORD OrderIdx, Vec3* Position, IUnit* Target)
 {
 	return true;
 }
 
-void AIO::OnOrbwalkAttack(IUnit* Source, IUnit* Target)
+PLUGIN_EVENT(void) OnBuffAdd(IUnit* Source, void* BuffData)
 {
 
 }
 
-void AIO::BeforeAttack(IUnit* Target)
+PLUGIN_EVENT(void) OnBuffRemove(IUnit* Source, void* BuffData)
 {
 
 }
 
-void AIO::OnInterruptible(InterruptibleSpell const& Args)
+PLUGIN_EVENT(void) OnGameEnd()
 {
 
 }
 
-void AIO::OnGapCloser(GapCloserSpell const& Args)
+PLUGIN_EVENT(void) OnLevelUp(IUnit* Source, int NewLevel)
 {
 
 }
 
-void AIO::OnLevelUp(IUnit* Source, int NewLevel)
+// Only called for local player, before the spell packet is sent
+PLUGIN_EVENT(void) OnPreCast(int Slot, IUnit* Target, Vec3* StartPosition, Vec3* EndPosition)
+{
+}
+
+PLUGIN_EVENT(void) OnDash(UnitDash* Args)
 {
 
 }
 
-void AIO::OnPlayAnimation(IUnit* Source, std::string const Args)
+PLUGIN_EVENT(void) OnD3DPresent(void* Direct3D9DevicePtr)
 {
 
 }
 
-void AIO::OnExitVisible(IUnit* Source)
+PLUGIN_EVENT(void) OnD3DPreReset(void* Direct3D9DevicePtr)
 {
 
 }
 
-void AIO::OnTeleport(OnTeleportArgs* Args)
+PLUGIN_EVENT(void) OnD3DPostReset(void* Direct3D9DevicePtr)
+{
+
+}
+
+PLUGIN_EVENT(void) OnRenderBehindHUD()
+{
+
+}
+
+// Return false to set this message as handled
+PLUGIN_EVENT(bool) OnWndProc(HWND Wnd, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	return true;
+}
+
+PLUGIN_EVENT(void) OnEnterVisible(IUnit* Source)
+{
+
+}
+
+PLUGIN_EVENT(void) OnExitVisible(IUnit* Source)
 {
 
 }
--- LeagueOfYeah/AIO.h
+++ LeagueOfYeah/AIO.h
@@ -7,19 +7,83 @@ public:
 	AIO(IUnit* Champion, IMenu* ChampMenu);
 	~AIO(); // De-constructor
 
-	virtual void OnGameUpdate();
-	virtual void OnRender();
-	virtual void OnRealSpellCast(CastedSpell const& Args);
-	virtual void OnSpellCast(CastedSpell const& Args);
-	virtual bool OnPreCast(int Slot, IUnit* Target, Vec3* StartPosition, Vec3* EndPosition);
-	virtual void OnOrbwalkAttack(IUnit* Source, IUnit* Target);
-	virtual void BeforeAttack(IUnit* Target);
-	virtual void OnInterruptible(InterruptibleSpell const& Args);
-	virtual void OnGapCloser(GapCloserSpell const& Args);
-	virtual void OnLevelUp(IUnit* Source, int NewLevel);
-	virtual void OnPlayAnimation(IUnit* Source, std::string const Args);
-	virtual void OnExitVisible(IUnit* Source);
-	virtual void OnTeleport(OnTeleportArgs* Args);
+	virtual void OnOrbwalkAttack(IUnit* Source, IUnit* Target)
+
+	virtual void  OnOrbwalkAfterAttack(IUnit* Source, IUnit* Target)
+
+
+	// Return an IUnit object here to force the orbwalker to select it for this tick
+	virtual void  OnOrbwalkingFindTarget()
+
+
+	virtual void OnOrbwalkTargetChange(IUnit* OldTarget, IUnit* NewTarget)
+
+
+	virtual void OnOrbwalkNonKillableMinion(IUnit* NonKillableMinion)
+
+	virtual void OnGameUpdate()
+
+	virtual void OnRender()	
+
+	virtual void OnSpellCast(CastedSpell const& Args)
+
+
+	virtual void OnUnitDeath(IUnit* Source)
+
+
+	virtual void OnCreateObject(IUnit* Source)
+
+
+	virtual void OnDestroyObject(IUnit* Source)
+
+
+	virtual void  OnDoCast(CastedSpell const& Args)
+
+	virtual void OnInterruptible(InterruptibleSpell const& Args)
+
+
+	virtual void OnGapCloser(GapCloserSpell const& Args)
+
+
+	// Called when issuing an order (e.g move, attack, etc.)
+	// Return false to stop order from executing
+	virtual void OnIssueOrder(IUnit* Source, DWORD OrderIdx, Vec3* Position, IUnit* Target)
+
+
+	virtual void OnBuffAdd(IUnit* Source, void* BuffData)
+
+	virtual void OnBuffRemove(IUnit* Source, void* BuffData)
+
+
+	virtual void OnGameEnd()
+
+
+	virtual void OnLevelUp(IUnit* Source, int NewLevel)
+
+
+	// Only called for local player, before the spell packet is sent
+	virtual void OnPreCast(int Slot, IUnit* Target, Vec3* StartPosition, Vec3* EndPosition)
+
+	virtual void OnDash(UnitDash* Args)
+
+	virtual void OnD3DPresent(void* Direct3D9DevicePtr)
+
+
+	virtual void OnD3DPreReset(void* Direct3D9DevicePtr)
+
+
+	virtual void OnD3DPostReset(void* Direct3D9DevicePtr)
+
+	virtual void OnRenderBehindHUD()
+
+
+	// Return false to set this message as handled
+	virtual void OnWndProc(HWND Wnd, UINT Message, WPARAM wParam, LPARAM lParam)
+
+
+	virtual void OnEnterVisible(IUnit* Source)
+
+	virtual void OnExitVisible(IUnit* Source)
 
 private:
 	IUnit* Champion;
